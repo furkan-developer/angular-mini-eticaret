88819242f75eebe0600c972591030287763942d2
Furkan Aydin <furknn2112@gmail.com>
2023-02-03 16:57:34 +0300

    Fix : static productlist template
    Bu commit'ten önce, ürünler template de static şekilde oluşuyor.
    
    Bu commit, ürünleri productlist componentin ilgili .ts uzantılı
    dosyasından alıp dinamik bir şekilde ürünleri template de oluşturur.

--- src/app/productlist/productlist.component.ts
+++ src/app/productlist/productlist.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { identifierName } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-productlist',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class ProductlistComponent {
 
+  title: String = "ShoopApp";
+
+  private products: any[] = [
+    {id: 1, productName: "Product 1", price: 20000, imgUrl: "../../assets/phone.jpg", isActive: true, description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe incidunt est velit illum aliquid voluptatibus a officiis numquam mollitia quisquam?"},
+    {id: 2, productName: "Product 2", price: 30000, imgUrl: "../../assets/phone.jpg", isActive: false, description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe incidunt est velit illum aliquid voluptatibus a officiis numquam mollitia quisquam?"},
+    {id: 3, productName: "Product 3", price: 40000, imgUrl: "../../assets/phone.jpg", isActive: true, description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe incidunt est velit illum aliquid voluptatibus a officiis numquam mollitia quisquam?"},
+    {id: 4, productName: "Product 4", price: 50000, imgUrl: "../../assets/phone.jpg", isActive: true, description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe incidunt est velit illum aliquid voluptatibus a officiis numquam mollitia quisquam?"},
+    {id: 4, productName: "Product 4", price: 50000, imgUrl: "../../assets/phone.jpg", isActive: true, description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe incidunt est velit illum aliquid voluptatibus a officiis numquam mollitia quisquam?"},
+  ]
+
+  getProducts():any[]{
+    return this.products.filter(p => p.isActive);
+  }
 }
