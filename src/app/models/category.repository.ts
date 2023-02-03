c691f2578dd71dfaf848449240909c9a766f132f
Furkan Aydin <furknn2112@gmail.com>
2023-02-03 18:25:26 +0300

    Create : CategoryRepository

---
+++ src/app/models/category.repository.ts
@@ -0,0 +1,16 @@
+import { Category } from "./category";
+
+export class CategoryRepository{
+
+  private categories: Category[] = [
+    {id: 1, name: "Category 1"},
+    {id: 2, name: "Category 2"},
+    {id: 3, name: "Category 3"},
+    {id: 4, name: "Category 4"},
+  ]
+
+  getProducts():Category[]{
+    return this.categories;
+  }
+
+}
