239be272a0be9e1fa9c3c244a641e731d5b9f35a
Furkan Aydin <furknn2112@gmail.com>
2023-02-03 17:55:50 +0300

    Apply : Product model in productlist.component.ts file

--- src/app/productlist/productlist.component.ts
+++ src/app/productlist/productlist.component.ts
@@ -1,5 +1,6 @@
 import { identifierName } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-productlist',
@@ -10,7 +11,7 @@ export class ProductlistComponent {
 
   title: String = "ShoopApp";
 
-  private products: any[] = [
+  private products: Product[] = [
     {id: 1, productName: "Product 1", price: 20000, imgUrl: "../../assets/phone.jpg", isActive: true, description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe incidunt est velit illum aliquid voluptatibus a officiis numquam mollitia quisquam?"},
     {id: 2, productName: "Product 2", price: 30000, imgUrl: "../../assets/phone.jpg", isActive: false, description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe incidunt est velit illum aliquid voluptatibus a officiis numquam mollitia quisquam?"},
     {id: 3, productName: "Product 3", price: 40000, imgUrl: "../../assets/phone.jpg", isActive: true, description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe incidunt est velit illum aliquid voluptatibus a officiis numquam mollitia quisquam?"},
@@ -18,7 +19,7 @@ export class ProductlistComponent {
     {id: 4, productName: "Product 4", price: 50000, imgUrl: "../../assets/phone.jpg", isActive: true, description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe incidunt est velit illum aliquid voluptatibus a officiis numquam mollitia quisquam?"},
   ]
 
-  getProducts():any[]{
+  getProducts():Product[]{
     return this.products.filter(p => p.isActive);
   }
 }
