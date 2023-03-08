1eccb4fd9a4ccd2c75657d21b80ee8ca9ae4d77c
Furkan Aydin <furknn2112@gmail.com>
2023-03-08 18:51:34 +0300

    APPLY: routing shemas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { ProductComponent } from './product/product.component';
+import { ErrorComponent } from './error/error.component';
 
-
+// Create routing shema
+const routers: Routes = [
+  {path:"", component: HomeComponent},
+  {path:"productlist", component: ProductlistComponent},
+  {path:"**", component: ErrorComponent}
+];
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    // Passed routing shema to Angular RouterModule
+    RouterModule.forRoot(routers)
+  ],
+  // Exported RouterModule for other module
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
