699c17ff14236471be8137b236b54c6fba75f417
Furkan Aydin <furknn2112@gmail.com>
2023-03-09 14:05:37 +0300

    ADD: add product action in navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  addProduct(){
+    let product = {id: 9, productName: "Product 9", price: 50000, imgUrl: "../../assets/phone.jpg", isActive: true, description: "Lorem ipsum, dolor",categoryId:3};
+
+    this.http.post("https://ng-onlineshop-default-rtdb.firebaseio.com/product.json",product).subscribe(
+      (recievedData)=>{
+        console.log(recievedData);
+      }
+    );
+  }
 }
