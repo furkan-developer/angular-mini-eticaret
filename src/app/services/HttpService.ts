3797e352206068c53e744f3ebfbcca9589ca3b66
furkan-developer <68426648+furkan-developer@users.noreply.github.com>
2023-03-12 11:51:21 +0300

    MODIFY: getAllProducts and getOneProduct method flow with Observable<Object>.pipe()
     # pipe metodu gelen datayı işlemek için çok kullanışlı operatorlere sahiptir.
     # map operatörü ile data işlenip return ediliyor.

--- src/app/services/HttpService.ts
+++ src/app/services/HttpService.ts
@@ -1,20 +1,45 @@
 import { HttpClient } from "@angular/common/http";
+import { ReturnStatement } from "@angular/compiler";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { map, Observable } from "rxjs";
 import { Product } from "../models/product";
+import { ProductInsertModel } from "../models/ProductInsertModel";
 
 @Injectable()
 export class HttpService{
 
-  url: string = "https://ng-onlineshop-default-rtdb.firebaseio.com/product.json";
+  url: string = "https://ng-onlineshop-default-rtdb.firebaseio.com";
   constructor(private http: HttpClient) {
   }
 
-  insertProduct(product: Product):Observable<object>{
-    return this.http.post(this.url,product);
+  insertProduct(product: ProductInsertModel):Observable<object>{
+    return this.http.post(`${this.url}/product.json`,product);
   }
 
-  getAllProducts():Observable<object>{
-    return this.http.get(this.url);
+  getAllProducts(categoryId: number):Observable<Product[]>{
+    return this.http.get(`${this.url}/product.json`).pipe(map((data)=>{
+      let products: Product[] = [];
+
+      for(const key in data){
+        if(categoryId && data[key].categoryId != categoryId)
+          continue;
+
+        let product = {...data[key]};
+        product.id = key;
+        products.push(product);
+      }
+      return products;
+    }));
+  }
+
+  getOneProducts(firebaseId: string):Observable<Product>{
+    return this.http.get<Product>(`${this.url}/product/${firebaseId}.json`).pipe(map((data)=>{
+      let product: Product;
+
+      product = data;
+      product.id = firebaseId;
+
+      return product;
+    }))
   }
 }
